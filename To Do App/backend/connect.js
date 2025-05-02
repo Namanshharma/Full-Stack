2f7da5c43be35a8e195b395fbf4b5b720f926f0c
Naman Sharma <namansharma812@gmail.com>
2025-05-02 14:00:52 +0530

    Minor code changes in the To Do Schema

--- To Do App/backend/connect.js
+++ To Do App/backend/connect.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
-const { boolean } = require('zod');
 
-const connectToMongo = () => {
-    return mongoose.connect("mongodb://127.0.0.1:27017/ToDos").then(() => console.log("Connected with Mongo DB")).catch(() => console.log("Something went wrong"));
+const connectToMongo = async () => {
+    return await mongoose.connect("mongodb://127.0.0.1:27017/ToDos")
+        .then(() => console.log("Connected with Mongo DB"))
+        .catch(() => console.log("Something went wrong"));
 }
 
 const todoSchema = new mongoose.Schema({
-    title: String, description: String, completed: boolean
+    title: String, description: String, completed: Boolean
 }, { timestamps: true })
 
 const ToDo = mongoose.model("ToDo", todoSchema);
